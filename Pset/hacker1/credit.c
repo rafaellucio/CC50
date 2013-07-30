8089039ed2ee73a24f06bab363ed4977836036b5
rafaellucio <rafaelantoniolucio@gmail.com>
2013-07-30 00:05:33 -0300

    create initial Validate Credit

---
+++ Pset/hacker1/credit.c
@@ -0,0 +1,20 @@
+/***************************************************************************
+ * skittles.c
+ *
+ * CC50
+ * Pset Hacker 1
+ *
+ * Validando Cartões de Credito.
+ *
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+	printf("***** Credito Furado ******");
+	printf("\nVamos validar seu cartão de credito!!");
+	printf("\n\nInforme o numero do seu cartao! ATENCAO somente numeros!");
+	printf("\n\nNumeros do cartao: ");
+}
